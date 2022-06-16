2c7aa66fa29c45a2fd1bd34e7e5ab9e9ff6c5c0a
Manasa Bingi <67860501+manasabingi115@users.noreply.github.com>
2022-06-16 18:01:36 +0530

    working on navigation.

--- src/index.js
+++ src/index.js
@@ -12,9 +12,14 @@ dashboard.addEventListener("click", () => {
 });
 
 patients.addEventListener("click", () => {
-  console.log("hello");
   maincontent.innerHTML = `<div>
   <h2>Patient</h2>
+  <nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Patients</li>
+  </ol>
+  </nav>
   <input placeholder="Search"></input>
   <p>There are no patients yet, let's add the first one!</p>
   <button>New Patient</button>
